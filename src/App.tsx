c13d6a5f928b0ff20770404179111fc52ba6dba2
jackcolin21307 <jackcolin21307@gmail.com>
2022-06-24 13:19:52 +0300

    Implements get-requests using react-query and axios

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import "bootstrap/dist/css/bootstrap.min.css";
-
-const App: React.FC = () => {
-  return null;
-}
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import './index.css';
-import App from './App';
+import App from './components/App';
 
 const queryClient = new QueryClient();
 
