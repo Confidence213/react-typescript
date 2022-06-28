e0e75a56ec313ec6be95e9fbaf14ccbb10aa1396
jackcolin21307 <jackcolin21307@gmail.com>
2022-06-28 09:16:11 +0300

    Creates form markup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import Container from "./Container";
 import SearchForm from "./SearchForm";
+import ArticleForm from "./ArticleForm";
 
 const App: React.FC = () => {
   return (
     <Container>
       <SearchForm />
+      <ArticleForm />
     </Container>
   );
 };
--- src/components/ArticleForm/ArticleForm.tsx
+++ src/components/ArticleForm/ArticleForm.tsx
@@ -1,15 +1,36 @@
-import React, { useState, useEffect } from "react";
-import { useMutation } from "react-query";
+// import React, { useState, useEffect } from "react";
+// import { useMutation } from "react-query";
 
-import IArticle from "../../types/Article";
-import articleService from "../../services/apiService";
+// import IArticle from "../../types/Article";
+// import articleService from "../../services/apiService";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const ArticleForm: React.FC = () => {
-  const [state, setState] = useState("");
+  // const [state, setState] = useState("");
 
-  return <div className="card"></div>;
+  return (
+    <div className="card">
+      <div className="card-header">Create a new article</div>
+      <div className="card-body">
+        <div className="form-group">
+          <input type="text" className="form-control" placeholder="Title" />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Description"
+          />
+        </div>
+        <button className="btn btn-sm btn-primary">Post Data</button>
+        <button className="btn btn-sm btn-warning ml-2">Clear</button>
+        <div className="alert alert-secondary mt-2" role="alert">
+          <pre>Result</pre>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ArticleForm;
