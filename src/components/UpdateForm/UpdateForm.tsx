3684059cc12ef7f1d41299ac3ed8e3079b1c4b4c
jackcolin21307 <jackcolin21307@gmail.com>
2022-06-29 09:25:00 +0300

    Creates markup and forms controlled elements

--- src/components/UpdateForm/UpdateForm.tsx
+++ src/components/UpdateForm/UpdateForm.tsx
@@ -6,7 +6,63 @@ import articleService from "../../services/apiService";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const UpdateForm: React.FC = () => {
-  return <div className="card"></div>;
+  const [id, setId] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [published, setPublished] = useState(false);
+  const [result, setResult] = useState<string | null>(null);
+
+  return (
+    <div className="card">
+      <div className="card-header">Update your article</div>
+      <div className="card-body">
+        <div className="form-group">
+          <input
+            type="text"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+            className="form-control"
+            placeholder="Id"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+            placeholder="Title"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            className="form-control"
+            placeholder="Description"
+          />
+        </div>
+        <div className="form-check mb-2">
+          <input
+            type="checkbox"
+            name="Published"
+            checked={published}
+            onChange={(e) => setPublished(e.target.checked)}
+            className="form-check-input"
+          />
+          <label className="form-check-label" htmlFor="Published">
+            Publish
+          </label>
+        </div>
+        <button className="btn btn-sm btn-primary">Update Data</button>
+        <button className="btn btn-sm btn-warning ml-2">Clear</button>
+        <div className="alert alert-secondary mt-2" role="alert">
+          <pre>Result</pre>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UpdateForm;
