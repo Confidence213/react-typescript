ab86f059d56a5c11e18742141ec21712eb154c54
jackcolin21307 <jackcolin21307@gmail.com>
2022-06-30 09:09:54 +0300

    Creates markup and forms controlled elements

--- src/components/DeleteForm/DeleteForm.tsx
+++ src/components/DeleteForm/DeleteForm.tsx
@@ -6,9 +6,32 @@ import articleService from "../../services/apiService";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const DeleteForm: React.FC = () => {
-  const [state, setState] = useState("");
+  const [id, setId] = useState("");
 
-  return <div className="card"></div>;
+  return (
+    <div className="card">
+      <div className="card-header">Delete your article</div>
+      <div className="card-body">
+        <div className="input-group input-group-sm">
+          <button className="btn btn-sm btn-danger">Delete All</button>
+          <input
+            type="text"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+            className="form-control ml-2"
+            placeholder="Id"
+          />
+          <div className="input-group-append">
+            <button className="btn btn-sm btn-danger">Delete by Id</button>
+          </div>
+          <button className="btn btn-sm btn-warning ml-2">Clear</button>
+        </div>
+        <div className="alert alert-secondary mt-2" role="alert">
+          <pre>Result</pre>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DeleteForm;
